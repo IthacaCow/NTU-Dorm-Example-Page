f0cd00755c15a1654476c37bf7d99f9cf6470575
IthacaCow <cba840910@gmail.com>
2015-07-28 23:33:22 +0800

    modify url path

--- js/nav-controllers.js
+++ js/nav-controllers.js
@@ -11,57 +11,57 @@ var model = {
 	{ name: 'Quota',
 	  type: 'toggle',
 	  children:[
-	      { name: '排名', type: 'link',url: '#' },
-	      { name: '日排名', type: 'link',url: '#' }
+	      { name: '排名', type: 'link',url: '#/' },
+	      { name: '日排名', type: 'link',url: '#/' }
 	  ]
 	}, 
 	{ name: '異常',
 	  type: 'toggle',
 	  children:[
-	      { name: '列表＆新增', type: 'link',url: '#' },
-	      { name: '歷史列表', type: 'link',url: '#' },
-	      { name: '未註冊列表', type: 'link',url: '#' },
-	      { name: '未註冊歷史列表', type: 'link',url: '#' },
-	      { name: '臨界值列表', type: 'link',url: '#' }
+	      { name: '列表＆新增', type: 'link',url: '#/' },
+	      { name: '歷史列表', type: 'link',url: '#/' },
+	      { name: '未註冊列表', type: 'link',url: '#/' },
+	      { name: '未註冊歷史列表', type: 'link',url: '#/' },
+	      { name: '臨界值列表', type: 'link',url: '#/' }
 	  ]
 	},
 	{ name: '權限',
 	  type: 'toggle',
 	  children:[
-	      { name: '修改密碼與資料', type: 'link',url: '#' }
+	      { name: '修改密碼與資料', type: 'link',url: '#/' }
 	  ]
 	},
 	{ name: '住宿',
 	  type: 'toggle',
 	  children:[
-	      { name: '暑宿搬遷列表&新增', type: 'link',url: '#' }
+	      { name: '暑宿搬遷列表&新增', type: 'link',url: '#/' }
 	  ]
 	},
 	{ name: '同步',
 	  type: 'toggle',
 	  children:[
-	      { name: 'Router', type: 'link',url: '#'}
+	      { name: 'Router', type: 'link',url: '#/'}
 	  ]
 	},
 	{ name: 'IP, Mac對應表',
 	  type: 'toggle',
 	  children:[		      
 	      { name: '列表&新增', type: 'link',url: '/#/users' },
-	      { name: '未使用IP列表', type: 'link',url: '#' },
-	      { name: '申請列表', type: 'link',url: '#' }		       
+	      { name: '未使用IP列表', type: 'link',url: '#/' },
+	      { name: '申請列表', type: 'link',url: '#/' }		       
 	  ]
 	},
 	{ name: 'VLAN',
 	  type: 'toggle',
 	  children:[		      
-	      { name: 'DHCP查詢', type: 'link',url: '#' }
+	      { name: 'DHCP查詢', type: 'link',url: '#/' }
 	  ]
 	},
 	{ name: '組態',
 	  type: 'toggle',
 	  children:[		      
-	      { name: 'MAC搜尋', type: 'link',url: '#' },
-	      { name: 'IP搜尋', type: 'link',url: '#' }
+	      { name: 'MAC搜尋', type: 'link',url: '#/' },
+	      { name: 'IP搜尋', type: 'link',url: '#/' }
 	  ]
 	}
     ]
